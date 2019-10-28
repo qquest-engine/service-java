56a86174eaa8af7efaedd437b8f033f6b431bad8
akuzmenko <al.kuzmm@gmail.com>
2019-10-28 18:28:18 +0200

    init structure

---
+++ src/test/java/ua/ithillel/evo/questengine/QuestEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ithillel.evo.questengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
