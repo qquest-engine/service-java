6a0defda8b5a7a316a3d74690118642b26a97563
akuzmenko <al.kuzmm@gmail.com>
2019-11-15 10:09:51 +0200

    add WebMvc annotation

--- src/main/java/ua/ithillel/evo/questengine/QuestEngineApplication.java
+++ src/main/java/ua/ithillel/evo/questengine/QuestEngineApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableSwagger2
+@EnableWebMvc
 public class QuestEngineApplication {
 
     @Bean
