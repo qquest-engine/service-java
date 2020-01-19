59e7fc4d9bc9155ede9757593c6cbe19b2705b65
Ваше Имя <you@example.com>
2020-01-19 14:55:23 +0200

    change to Bearer authentication

--- src/test/java/ua/ithillel/evo/questengine/web/controller/QuestControllerIntegrationTest.java
+++ src/test/java/ua/ithillel/evo/questengine/web/controller/QuestControllerIntegrationTest.java
@@ -3,16 +3,26 @@ package ua.ithillel.evo.questengine.web.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
+import ua.ithillel.evo.questengine.data.entity.Quest;
+import ua.ithillel.evo.questengine.data.entity.Type;
+import ua.ithillel.evo.questengine.data.entity.User;
 import ua.ithillel.evo.questengine.service.QuestService;
+import ua.ithillel.evo.questengine.service.UserService;
+
+import java.util.List;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class QuestControllerIntegrationTest extends ControllerBaseTest {
+class QuestControllerIntegrationTest extends ControllerBaseTest {
 
     @Autowired
     QuestService questService;
 
+    @Autowired
+    UserService userService;
+
     @Test
     void whenCreateQuestThenRespondCreatedTest() {
+
 
     }
 }
