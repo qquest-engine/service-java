cb594050da76e2fa0f4cc085013c3e8b542d673c
akuzmenko <al.kuzmm@gmail.com>
2020-01-20 12:11:05 +0200

    add WebMvcConfigurerAdapter

--- src/main/java/ua/ithillel/evo/questengine/configuration/WebConfig.java
+++ src/main/java/ua/ithillel/evo/questengine/configuration/WebConfig.java
@@ -1,23 +1,16 @@
-//package ua.ithillel.evo.questengine.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.header.writers.StaticHeadersWriter;
-//
-//@Configuration
-//public class WebConfig {
-//
-//    @Bean
-//    public WebSecurityConfigurerAdapter webSecurity() {
-//        return new WebSecurityConfigurerAdapter() {
-//
-//            @Override
-//            protected void configure(HttpSecurity http) throws Exception {
-//                http.headers().addHeaderWriter(
-//                        new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
-//            }
-//        };
-//    }
-//}
+package ua.ithillel.evo.questengine.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
